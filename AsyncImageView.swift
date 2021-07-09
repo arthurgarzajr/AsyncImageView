dc23b0d98eaab47f73feea1b7b5c231db3cd626f
Arthur Garza <arthur.garzajr@gmail.com>
2021-07-09 15:41:31 -0500

    AsyncImageView.swift

---
+++ AsyncImageView.swift
@@ -0,0 +1,88 @@
+import SwiftUI
+
+struct AsyncImageView: View {
+    @ObservedObject var viewModel: AsyncImageViewModel
+    
+    var body: some View {
+        if viewModel.downloadingImage {
+            ActivityIndicatorView()
+        } else if let image = viewModel.image {
+            Image(uiImage: image)
+                .resizable()
+                .foregroundColor(.secondary)
+                .clipShape(Circle())
+                .shadow(radius: 10)
+        } else {
+            Image(systemName: "person.crop.circle.fill")
+                .resizable()
+                .foregroundColor(.secondary)
+                .clipShape(Circle())
+                .shadow(radius: 10)
+        }
+    }
+}
+
+import Foundation
+import UIKit
+
+class AsyncImageViewModel: ObservableObject {
+    @Published var image: UIImage?
+    
+    var urlString: String?
+    
+    @Published var downloadingImage = false
+    
+    var serviceAPI: ServiceAPIProtocol?
+    
+    init(urlString: String?, serviceAPI: ServiceAPIProtocol?) {
+        self.urlString = urlString
+        self.serviceAPI = serviceAPI
+        
+        guard let urlString = urlString else {
+            return
+        }
+        
+        #if !TEST
+        fetchImage(for: urlString)
+        #endif
+    }
+    
+    func fetchImage(for urlString: String) {
+        guard let url = URL(string: urlString) else {
+            downloadingImage = false
+            return
+        }
+        
+        let imageName = ImageStorageHandler.getImageName(from: url)
+        
+        // Check cache for the image.
+        if let locallyCachedImage = ImageStorageHandler.getImageFromLocalStorage(imageName: imageName) {
+            image = locallyCachedImage
+            return
+        }
+        
+        downloadImage(url: url)
+    }
+    
+    func downloadImage(url: URL) {
+        downloadingImage = true
+        
+        // Download image
+        serviceAPI?.fetchImage(for: url) { result in
+            switch result {
+            case .success(let image):
+                DispatchQueue.main.async {
+                    self.image = image
+                }
+                
+            case .failure(let error):
+                self.image = UIImage(systemName: "person.crop.circle.fill")
+                Logger.log(message: error.localizedDescription, severity: .warning, customData: [:])
+            }
+            
+            DispatchQueue.main.async {
+                self.downloadingImage = false
+            }
+        }
+    }
+}
